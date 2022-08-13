6100df6eaf9c5f20d41ac5669b525a96223c027e
Luis Alaguna <alagunasalahaddin@live.com>
2022-08-13 21:30:46 +0300

    :wrench: Getting admin tokens from environment files.

--- config/admin.ts
+++ config/admin.ts
@@ -2,5 +2,8 @@ export default ({ env }) => ({
   auth: {
     secret: env("ADMIN_JWT_SECRET", "b2666ced99728af340b6b337974466f7"),
   },
+  apiToken: {
+    salt: env("API_TOKEN_SALT"),
+  },
   watchIgnoreFiles: ["**/config/sync/**"],
 });
